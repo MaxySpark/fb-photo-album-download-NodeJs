7bc04b019548d529e79005b873f27c97fa339a4b
MaxySpark <maxyspark@gmail.com>
2016-07-28 19:26:41 +0530

    update app.js and add yargs

--- app.js
+++ app.js
@@ -2,14 +2,29 @@ var _ = require('underscore');
 var request = require('request');
 var FB = require('fb');
 var fs = require('fs');
+var argv = require('yargs').option({
+    album : {
+        //demand : true,
+        alias:[ 'n','a'],
+        description: "Name Of The Album",
+        type: 'string'
+    },
+    friend : {
+        alias: 'f',
+        description: "Name Of The friend",
+        type: 'string'
+    }
+}).help('help').alias('help','h').argv
 
-var my_token = "token";
+var my_token = "EAACEdEose0cBABroAioZBGzqRnpoakTvYrBN4tezypHxCvsIzmYH6WrIW7fbz6AbTwzwF3SQDPyXINZC65IJZBhZA69Nom6PmfK9UeWSfD2XTyeyiRyMIJ6AbTzAcmPqfIKTwTXfX5f7OxqvOcShk8ygVuhyCWoKmKPAtKFNtwZDZD";
 
 FB.setAccessToken(my_token);
 var urls = [];
 var count = 1;
 var i = 0;
 var j = 0;
+var album_name = argv.album || "Mobile Uploads";
+
 function downloadLoop(urls) {
             for(i;i<urls.length;i++){
                 request(urls[i]).pipe(fs.createWriteStream('img/'+(i+1)+'.jpg')).on('finish', function(response) {         
@@ -46,13 +61,14 @@ FB.api('/me/albums','get',  function (res) {
         return;
         }
        
-        var data = _.findWhere(res.data, {name:"Timeline Photos"});
+        var data = _.findWhere(res.data, {name:album_name});
         var nextPage = '/'+data.id+'/photos';
 
         FB.api(nextPage, function (response){
             for (var i = 0; i < response.data.length; i++) {
                 urls.push(response.data[i].images[0].source);                           
             }
+             console.log("Getting images... Total Image Found : "+ urls.length);
             download(response.paging.next);
         });        
     });
